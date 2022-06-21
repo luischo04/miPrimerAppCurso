3474708632d4ca97dea897a87ddb09145c0b6373
luischo04 <buenrostroluisfernando04@gmail.com>
2022-06-21 11:37:17 -0500

    ngIf
    Se comienza a usar directivas como ngIf y else.
    Ademas de ngTemplate para mostrar el contenido de else.

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -6,5 +6,25 @@ import { Component } from "@angular/core";
     styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent{
+    deshabilitar = false;
+    mensaje = '';
+    titulo = 'Ingeniero';
+    mostrar = false;
     
+    agregarPersona(){
+        this.mostrar = true;
+        this.mensaje = 'Persona agregada';
+    }
+
+    // Variable tipo Event
+    /**
+     * 
+     * @param event -> nos permite acceder a nuestro input
+     *  HTMLInputElement -> declara que es un input de html
+     */
+    // modificarTitulo(event: Event){
+    //     console.log('Entrando a metodo modificar titulo')
+    //     this.titulo = (<HTMLInputElement>event.target).value
+    // }
+
 }
